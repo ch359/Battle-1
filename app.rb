9a4c0b7ce23921c7363ba1c09d0e068aa00bf09b
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-23 16:27:13 +0000

    POST/redirect/GET session

--- app.rb
+++ app.rb
@@ -2,14 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
-    # 'Testing infrastructure working!'
     erb :index
   end
 
   post '/names' do
-    @player_one = params[:player_one]
-    @player_two = params[:player_two]
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    redirect "/names"
+  end
+
+  get '/names' do
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
     erb(:names)
   end
 
