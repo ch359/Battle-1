be02cefd6402747e0aad98671de2e5873ba5ed4f
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-23 15:32:56 +0000

    player one & player two can enter names

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -8,6 +8,10 @@ feature 'homepage ' do
 
   scenario " expects players to fill in their names via a form" do
     visit("/")
-    fill_in('name', with: 'John')
+    fill_in('player_one', with: 'John')
+    fill_in('player_two', with: 'James')
+    
+    click_button('Submit')
+    expect(page).to have_content("John")
   end
 end
