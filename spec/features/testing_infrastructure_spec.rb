a77e9fb2022feeb36a62951446aa11afe89bbd2f
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-23 14:52:41 +0000

    Passing test for a form on index

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,9 +1,13 @@
 require_relative '../../app.rb'
 
-feature 'homepage ' do 
-  scenario " says testing infrastructure working!" do 
-    visit("/")
-    expect(page).to have_content "Testing infrastructure working!"
-  end 
-end 
+feature 'homepage ' do
+  # scenario " says testing infrastructure working!" do
+  #   visit("/")
+  #   expect(page).to have_content "Testing infrastructure working!"
+  # end
 
+  scenario " expects players to fill in their names via a form" do
+    visit("/")
+    fill_in('name', with: 'John')
+  end
+end
