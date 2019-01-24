90b9cdc2ebbe0870054257719d843856633f87dc
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-24 14:49:46 +0000

    added spec helper file

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../../app.rb'
 
+
 feature 'homepage ' do
   # scenario " says testing infrastructure working!" do
   #   visit("/")
@@ -7,11 +8,7 @@ feature 'homepage ' do
   # end
 
   scenario " expects players to fill in their names via a form" do
-    visit("/")
-    fill_in('player_one', with: 'John')
-    fill_in('player_two', with: 'James')
-    
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content("John")
   end
 
